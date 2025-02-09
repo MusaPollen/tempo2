ad0825a40d59c7bfbc78a56763031db4a98722ba
MusaPollen <musa.pollen.light@gmail.com>
2025-02-09 14:06:47 +0600

    modification testing with logo

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -54,7 +54,7 @@ const Video = () => {
         autoplay={true}
         start={true}
         isOpen={isOpen}
-        videoId="L61p2uyiMSo"
+        videoId="yWd4mzGqQYo"
         onClose={() => setOpen(false)}
       />
 
